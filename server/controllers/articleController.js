b760a4ca5fbae87910ed940c8d4a6cb2080170c8
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-26 23:25:09 +0000

    structure fixes

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -48,6 +48,11 @@ const getArticle = AsyncHandler(async (req, res) => {
 const getUserArticles = AsyncHandler(async (req, res) => {
 	const userId = req.params.userId;
 
+	const user = await User.findById(userId);
+	if (!user) {
+		return res.status(404).json({ message: "User not found." });
+	}
+
 	const page = Number(req.query.page) || 1;
 	const limit = Number(req.query.limit) || 12;
 	const skip = (page - 1) * limit;
