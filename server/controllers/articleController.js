06aeef798995f4c110f25748481bea5d57affc4d
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-26 01:26:15 +0100

    edit article permition

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -102,7 +102,7 @@ const updateArticle = AsyncHandler(async (req, res) => {
 
 	const article = await Article.findById(articleId);
 	if (article) {
-		if (article.author.toString() !== userId.toString()) {
+		if (article.author.toString() !== userId.toString() && !req.user.isAdmin) {
 			return res.status(403).json({ message: "You don't have permission to update this article." });
 		}
 
@@ -138,7 +138,7 @@ const deleteArticle = AsyncHandler(async (req, res) => {
 
 	const article = await Article.findById(articleId);
 	if (article) {
-		if (article.author.toString() !== userId.toString()) {
+		if (article.author.toString() !== userId.toString() && !req.user.isAdmin) {
 			return res.status(403).json({ message: "You don't have permission to delete this article." });
 		}
 
