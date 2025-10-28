3c6f1feced3e8b9c6364039ecccf31c687f84bba
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-28 23:44:59 +0000

    added images

--- client/src/assets/images-data.js
+++ client/src/assets/images-data.js
@@ -1,5 +1,5 @@
 import favicon from "./images/favicon.png";
-import upload_area from './images/upload_area.svg';
+import upload_area from './images/upload_area.png';
 import person_icon from './images/person_icon.svg';
 import email_icon from './images/email_icon.svg'
 import lock_icon from './images/lock_icon.svg'
