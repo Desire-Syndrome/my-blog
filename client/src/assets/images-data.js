e611152a78c6508504415786089b12f2c2ea27e0
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-28 20:36:58 +0000

    uploaded logo

--- client/src/assets/images-data.js
+++ client/src/assets/images-data.js
@@ -3,10 +3,11 @@ import upload_area from './images/upload_area.svg';
 import person_icon from './images/person_icon.svg';
 import email_icon from './images/email_icon.svg'
 import lock_icon from './images/lock_icon.svg'
+import logo from './images/logo.png'
 
 
 export const assetsImages = {
-    favicon, 
+    favicon, logo,
     upload_area, person_icon, email_icon, lock_icon,
     
 }
