b113f5d56588cd72b366aac29dcde0296711f766
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-28 21:33:47 +0000

    added icons

--- client/src/assets/images-data.js
+++ client/src/assets/images-data.js
@@ -8,10 +8,13 @@ import icon1 from './images/icon1.png'
 import icon2 from './images/icon2.png'
 import icon3 from './images/icon3.png'
 import icon4 from './images/icon4.png'
+import icon5 from './images/icon5.png'
+import icon6 from './images/icon6.png'
+import icon7 from './images/icon7.png'
 
 
 export const assetsImages = {
     favicon, logo,
     upload_area, person_icon, email_icon, lock_icon,
-    icon1, icon2, icon3, icon4
+    icon1, icon2, icon3, icon4, icon5, icon6, icon7
 }
