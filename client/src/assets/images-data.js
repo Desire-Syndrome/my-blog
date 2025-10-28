6739c187332dad0603ebc00158853542c90f9708
Desire-Syndrome <desiresyndrome@gmail.com>
2025-10-28 21:14:17 +0000

    added images

--- client/src/assets/images-data.js
+++ client/src/assets/images-data.js
@@ -4,10 +4,14 @@ import person_icon from './images/person_icon.svg';
 import email_icon from './images/email_icon.svg'
 import lock_icon from './images/lock_icon.svg'
 import logo from './images/logo.png'
+import icon1 from './images/icon1.png'
+import icon2 from './images/icon2.png'
+import icon3 from './images/icon3.png'
+import icon4 from './images/icon4.png'
 
 
 export const assetsImages = {
     favicon, logo,
     upload_area, person_icon, email_icon, lock_icon,
-    
+    icon1, icon2, icon3, icon4
 }
