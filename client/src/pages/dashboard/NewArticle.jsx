92268209ea7a6b625df7096c27f25a5b9593b895
Desire-Syndrome <desiresyndrome@gmail.com>
2025-11-27 14:05:52 +0000

    css fixes

--- client/src/pages/dashboard/NewArticle.jsx
+++ client/src/pages/dashboard/NewArticle.jsx
@@ -1,3 +1,13 @@
+import { articlesCategories } from '../../assets/mock-data'
+
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { articlePostAction } from '../../redux/actions/articleActions';
+
+import Quill from 'quill';
+
 
 const NewArticle = () => {
 
